b206dca199cfa03708167748ebbab2263b48f89b
Piotr Pałek <piotrpalek@gmail.com>
2019-02-08 09:07:45 +0100

    fix IE 11

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,24 @@
-const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
-        ie: "11",
-      },
-      useBuiltIns: "usage",
-    },
+module.exports = {
+  ignore: [
+    /\/core-js/,
   ],
-];
 
-module.exports = { presets };
+  sourceType: "unambiguous",
+
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          edge: "17",
+          firefox: "60",
+          chrome: "67",
+          safari: "11.1",
+          ie: "11",
+        },
+        useBuiltIns: "usage",
+        modules: false
+      },
+    ],
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const prod = mode === "production";
 
 module.exports = {
   entry: {
-    bundle: ["./src/main.js"]
+    bundle: ["whatwg-fetch", "./src/main.js"]
   },
   resolve: {
     extensions: [".js", ".html"]
