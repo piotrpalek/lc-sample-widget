4b5d88828ce8c18deee233ebc3518d136e0895f3
Piotr Pałek <piotrpalek@gmail.com>
2019-02-08 08:09:10 +0100

    transpile nm

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /node_modules/,
+//        exclude: /node_modules/,
         use: "babel-loader"
       },
       {
