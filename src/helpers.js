6f1ca4f3572f653f2aa8e1c9dabf7117c6c42f4c
Piotr Pałek <piotrpalek@gmail.com>
2019-02-13 13:51:12 +0100

    match multiple tracking numbers per message

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export const upsRegex = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/i;
+export const upsRegex = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/gi;
 //export const upsRegex = /(\b1Z[0-9A-Z]{16}\b)|(\b\d{12}\b)|(\bT\d{10}\b)|(\b\d{9}\b)/i;
 
 export function capitalize(string) {
