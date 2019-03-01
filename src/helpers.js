9b97ee597c5ff84e236bf5db8b0566e592ac67a2
Piotr Pałek <piotrpalek@gmail.com>
2019-03-01 12:07:57 +0100

    additional ups regex

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,5 @@
 export const upsRegex = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/gi;
+export const upsRegex2 = /^[kKJjY]{1}[0-9]{10}$/gi;
 //export const upsRegex = /(\b1Z[0-9A-Z]{16}\b)|(\b\d{12}\b)|(\bT\d{10}\b)|(\b\d{9}\b)/i;
 
 export function capitalize(string) {
